f91dfbe21144bc67b58621a62d4c58c8e9bc0408
Maria Eduarda Heck <54290646+Mariaehsb@users.noreply.github.com>
2022-11-18 17:33:40 -0300

    Update formCadastroEspecie.php

--- GreenGo-main/app/views/especies/formCadastroEspecie.php
+++ GreenGo-main/app/views/especies/formCadastroEspecie.php
@@ -9,22 +9,23 @@
 <body>
 <h2>Cadastro de especies</h2>
 
-<form action="/greengoespecie/Greengo_Especies-main/GreenGo-main(4)/GreenGo-main/app/controllers/EspecieController.php?action=create" method="POST">
+<form action="/GreenGo-main/app/controllers/EspecieController.php?action=create" method="POST">
 	Nome Popular: <input type="text" name="nomePop">
 	<br>
 	Nome Cientifico: <input type="text" name="nomeCie">
 	<br>
 	Descrição: <input type="text" name="descricao">
 	Imagem: <input type="text" name="imagem">
-	<br><br>
-
+	<br>
+	Ponto Especie: <input type="text" name="pontoEsp"> <br><br>
 	Atributos Especiais: <br><br>
 	Frutifera 	<input type="checkbox" name="frutifera" value="1"><br>
 	Tóxica 		<input type="checkbox" name="toxidade" value="1"><br>
-	Exótica	<input type="checkbox" name="exotica" value="1"><br>
+	Exótica	    <input type="checkbox" name="exotica" value="1"><br>
 	Rara		<input type="checkbox" name="raridade" value="1"><br>
-	Medicinal	<input type="checkbox" name="medicinal" value="1">
-	<br>
+	Medicinal	<input type="checkbox" name="medicinal" value="1"><br>
+	Comestivel  <input type="checkbox" name="comestivel" value="1"><br>
+
 
 	<input type="submit" value="Registrar">
 	<input type="reset" value="Limpar">
